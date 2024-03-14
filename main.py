da6a0e2a1e66b29f0f088d632dc4da79ca96206b
Ben Smith <bh2smith@gmail.com>
2024-03-14 13:28:09 +0100

    cleanup some sheet

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from time import sleep
 from google.cloud import pubsub_v1
 import os
 
+from message import near_message
 from query_pg import get_contract_addresses, get_token_details, pg_connect, near_query
+from util import partition_array
 
 load_dotenv()
 # Also need GOOGLE_APPLICATION_CREDENTIALS in env.
@@ -26,39 +28,14 @@ def publish_message(data: str):
     # print(f"Published message ID: {future.result()}")
 
 
-def contract_message(contract_address: str) -> str:
-    message_dict = {"contract": {"address": contract_address}}
-    return json.dumps(message_dict)
-
-
-def token_message(address: str, token_id: str, token_uri: str) -> str:
-    message_dict = {
-        "token": {"address": address, "token_id": token_id, "token_uri": token_uri}
-    }
-    return json.dumps(message_dict)
-
-
-def near_message(contract_id: str, token_id: str, minter: str) -> str:
-    message_dict = {
-        "contract_id": contract_id,
-        "token_ids": [token_id],
-        "minter": minter,
-    }
-    return json.dumps(message_dict)
-
-
-def chunk_list(lst, chunk_size):
-    return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
-
-
 if __name__ == "__main__":
-    limit = 300000
+    limit = 20000
     load_dotenv()
     pg_connection = pg_connect()
     tokens = near_query(pg_connection, limit)
-    for chunk in chunk_list(tokens, 25):
+    for chunk in partition_array(tokens, 25):
         print(f"Posting {len(chunk)} token requests", datetime.now())
-        for a, b, c in tokens:
+        for a, b, c in chunk:
             message = near_message(a, b, c)
             publish_message(message)
         sleep(1)
